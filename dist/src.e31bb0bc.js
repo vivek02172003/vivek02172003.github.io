ab5a980b38c4741c1517852d5e1a24d0c821183c
Vivek Bhandari <v_bhandari@outlook.com>
2023-06-09 12:20:29 -0400

    fix contact us

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -6966,7 +6966,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65451" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50671" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
